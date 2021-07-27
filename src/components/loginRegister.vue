a12bebe65427c468818fff295dc44399e9411641
alex <apachecordovax@gmail.com>
2021-07-27 13:01:51 +0200

    mise en place des formulaires d'authentification

---
+++ src/components/loginRegister.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input
+      v-if="tab == 'register'"
+      outlined
+      class="q-mb-md"
+      v-model="formData.name"
+      label="Name"
+    />
+    <q-input
+      outlined
+      class="q-mb-md"
+      type="email"
+      v-model="formData.email"
+      label="Email"
+    />
+    <q-input
+      outlined
+      class="q-mb-md"
+      type="password"
+      v-model="formData.password"
+      label="Password"
+    />
+    <div class="row">
+      <q-space></q-space>
+      <q-btn type="submit" color="primary" :label="tab" />
+    </div>
+  </q-form>
+</template>
+
+<script>
+export default {
+  props: ["tab"],
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.tab == "login") {
+        console.log("login the user");
+      } else {
+        console.log("register the user");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -1,11 +1,46 @@
 <template>
-  <q-page class="flex">
-    <h5>Auth page</h5>
+  <q-page class="flex q-pa-md">
+    <q-card class="full-width">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <loginRegister :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <loginRegister :tab="tab" />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import loginRegister from "./../components/loginRegister.vue";
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    loginRegister,
+  },
+  data() {
+    return {
+      tab: "login",
+    };
+  },
+});
 </script>
